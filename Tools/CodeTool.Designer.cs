5189e1d60ef842f14fbe87ccb9c50b34ebdc1c76
devgis <devgis@163.com>
2019-01-10 15:01:22 +0800

    MuiltFile Gererate
    MuiltFile Gererate

---
+++ Tools/CodeTool.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Tools
+{
+    partial class CodeTool
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btCopy = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btGenerate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btGenFile = new System.Windows.Forms.Button();
+            this.tbExtend = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btCopy, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(989, 516);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(497, 33);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(489, 460);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btCopy
+            // 
+            this.btCopy.Location = new System.Drawing.Point(497, 3);
+            this.btCopy.Name = "btCopy";
+            this.btCopy.Size = new System.Drawing.Size(75, 23);
+            this.btCopy.TabIndex = 3;
+            this.btCopy.Text = "复制";
+            this.btCopy.UseVisualStyleBackColor = true;
+            this.btCopy.Click += new System.EventHandler(this.btCopy_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(488, 460);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btGenerate
+            // 
+            this.btGenerate.Location = new System.Drawing.Point(3, 3);
+            this.btGenerate.Name = "btGenerate";
+            this.btGenerate.Size = new System.Drawing.Size(64, 24);
+            this.btGenerate.TabIndex = 2;
+            this.btGenerate.Text = "生成代码";
+            this.btGenerate.UseVisualStyleBackColor = true;
+            this.btGenerate.Click += new System.EventHandler(this.btGenerate_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbExtend);
+            this.panel1.Controls.Add(this.btGenFile);
+            this.panel1.Controls.Add(this.btGenerate);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(494, 30);
+            this.panel1.TabIndex = 4;
+            // 
+            // btGenFile
+            // 
+            this.btGenFile.Location = new System.Drawing.Point(129, 5);
+            this.btGenFile.Name = "btGenFile";
+            this.btGenFile.Size = new System.Drawing.Size(64, 24);
+            this.btGenFile.TabIndex = 3;
+            this.btGenFile.Text = "批量文件";
+            this.btGenFile.UseVisualStyleBackColor = true;
+            this.btGenFile.Click += new System.EventHandler(this.btGenFile_Click);
+            // 
+            // tbExtend
+            // 
+            this.tbExtend.Location = new System.Drawing.Point(73, 5);
+            this.tbExtend.Name = "tbExtend";
+            this.tbExtend.Size = new System.Drawing.Size(50, 21);
+            this.tbExtend.TabIndex = 4;
+            this.tbExtend.Text = "md";
+            // 
+            // CodeTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 516);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "CodeTool";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "代码生成器";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btGenerate;
+        private System.Windows.Forms.Button btCopy;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbExtend;
+        private System.Windows.Forms.Button btGenFile;
+    }
+}
+
---
+++ Tools/CodeTool.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tools
+{
+    public partial class CodeTool : Form
+    {
+        public CodeTool()
+        {
+            InitializeComponent();
+        }
+
+        private void btGenerate_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in textBox1.Lines)
+            {
+                string temp = s.Replace("  ", " ").Replace('\t',' ').TrimStart(' ');
+                string[] arr = temp.Split(' ');
+                sb.AppendLine("///<summary>");
+                sb.AppendLine("///"+s);
+                sb.AppendLine("///</summary>");
+                sb.AppendLine(string.Format("public string {0}",arr[0]));
+                sb.AppendLine("{");
+                sb.AppendLine("  get;");
+                sb.AppendLine("  set;");
+                sb.AppendLine("}");
+                sb.AppendLine("");
+            }
+
+            textBox2.Text = sb.ToString();
+        }
+
+        private void btCopy_Click(object sender, EventArgs e)
+        {
+            textBox2.Copy();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btGenFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbExtend.Text.Trim()))
+            {
+                MessageBox.Show("请填写文件后缀！");
+                tbExtend.Focus();
+                return;
+            }
+            FolderBrowserDialog sfd = new FolderBrowserDialog();
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                foreach (string s in textBox1.Lines)
+                {
+                    if(!string.IsNullOrEmpty(s))
+                    {
+                        try
+                        {
+                            File.Create(Path.Combine(sfd.SelectedPath, s + "." + tbExtend.Text));
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("发生异常:" + ex.Message);
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("执行成功！ ");
+           
+        }
+    }
+}
